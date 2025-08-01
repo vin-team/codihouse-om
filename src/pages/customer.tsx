e03b24f6b7f42b9840bbbfc697dc6dae7cedb196
htdo12 <thng.huy1202@gmail.com>
2025-08-01 11:23:30 +0700

    feat: implement header

--- src/pages/customer.tsx
+++ src/pages/customer.tsx
@@ -1,12 +1,17 @@
+import Layout from '@/components/dashboard/Layout';
 import React from 'react';
 
 const Customer: React.FC = () => {
-    return (
+  return (
+    <Layout
+      children={
         <div>
-            <h1>Welcome to Customer list</h1>
-            <p>This is the page of managing your customer.</p>
+          <h1>Welcome to Customer list</h1>
+          <p>This is the page of managing your customer.</p>
         </div>
-    );
+      } />
+
+  );
 };
 
 export default Customer;
