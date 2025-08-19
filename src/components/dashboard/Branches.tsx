72650f7a5e7f90e57a1dedcb2a3f477e09b7edcd
htdo12 <thng.huy1202@gmail.com>
2025-08-19 18:01:51 +0700

    Updated behavior when click on branches in dashboard

--- src/components/dashboard/Branches.tsx
+++ src/components/dashboard/Branches.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Badge } from "../ui/badge";
 import { Card, CardContent } from "../ui/card";
 import { useAppDispatch, useAppSelector } from "@/hooks/redux";
@@ -5,13 +6,19 @@ import { getBranches } from "@/slices/branchSlice";
 import { useEffect } from "react";
 import { LoaderCircle } from "lucide-react";
 import { getOrderCountByBranches } from "@/slices/orderSlice";
+import { useRouter } from "next/navigation";
 
 export default function Branches() {
+  const router = useRouter();
   const dispatch = useAppDispatch();
   const branches = useAppSelector(state => state.branch.branches);
   const requestState = useAppSelector(state => state.branch.requestState);
   const sliceBranches = branches.length > 5 ? branches.slice(0, 5) : branches;
   const orderCountByBranches = useAppSelector(state => state.order.orderCountByBranches);
+
+  const handleBranchClick = (branchId: number) => {
+    router.push(`/branches/edit/${branchId}`);
+  };
 
   useEffect(() => {
     dispatch(getBranches());
@@ -39,13 +46,13 @@ export default function Branches() {
         <div className="space-y-4">
           {sliceBranches.length > 0 && requestState.status !== 'loading' && sliceBranches.map((branch) => (
             <div key={branch.id} className="flex items-center justify-between">
-              <div>
+              <button role="button" onClick={() => handleBranchClick(branch.id)} className="flex flex-col items-start">
                 <p className="font-medium">{branch.title}</p>
                 <p className="text-sm text-gray-500">
                   {branch?.type ? `${branch.type.toUpperCase()} • ` : ""}
                   {orderCountByBranches.find(item => item.branch_id === branch.id)?.count || 0} đơn hôm nay
                 </p>
-              </div>
+              </button>
               {branch?.state && <Badge
                 className={`${branch.state === "active" ? "bg-green-500 text-white hover:bg-green-500" : "bg-red-500 text-white hover:bg-red-500"}`}
                 variant="secondary">{branch?.state?.toUpperCase()}</Badge>}
